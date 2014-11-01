2efc625dae077e52ddb46b5fb0f61058853c0cb2
Mitchell Wills <mwills@wpi.edu>
2014-11-01 16:55:34 -0400

    Added diagnostics to orientus node

--- orientus_driver/src/orientus_node.cpp
+++ orientus_driver/src/orientus_node.cpp
@@ -11,6 +11,8 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
 #include "geometry_msgs/Quaternion.h"
+#include "diagnostic_updater/diagnostic_updater.h"
+#include "diagnostic_updater/DiagnosticStatusWrapper.h"
 
 class OrientusNode {
 private:
@@ -19,8 +21,10 @@ private:
   ros::Rate loop_rate_;
   std::string frame_id_;
   ros::Publisher imu_pub_;
+  diagnostic_updater::Updater diagnostic_;
 
   boost::asio::io_service io_service_;
+  std::string port_name_;
   boost::asio::serial_port port_;
   an_decoder_t an_decoder_;
 
@@ -38,13 +42,12 @@ private:
 public:
   OrientusNode(ros::NodeHandle nh, ros::NodeHandle pnh)
     : nh_(nh), pnh_(pnh), loop_rate_(100),
-      port_(io_service_){
+      diagnostic_(nh_, pnh_), port_(io_service_){
 
-    std::string port_name;
-    pnh_.param("port", port_name, std::string("/dev/ttyUSB0"));
+    pnh_.param("port", port_name_, std::string("/dev/ttyUSB0"));
     pnh_.param("frame_id", frame_id_, std::string("imu"));
 
-    port_.open(port_name);
+    port_.open(port_name_);
     port_.set_option(boost::asio::serial_port_base::baud_rate(115200));
 
     an_packet_t *an_packet;
@@ -58,22 +61,33 @@ public:
     packet_periods_packet_t packet_periods_packet;
     packet_periods_packet.permanent = 0;
     packet_periods_packet.clear_existing_packets = 1;
-    packet_periods_packet.packet_periods[0].packet_id = packet_id_status;
-    packet_periods_packet.packet_periods[0].period = 100; // 10 Hz
-    packet_periods_packet.packet_periods[1].packet_id = packet_id_quaternion_orientation_standard_deviation;
+    packet_periods_packet.packet_periods[0].packet_id = packet_id_quaternion_orientation_standard_deviation;
+    packet_periods_packet.packet_periods[0].period = 20; // 50 Hz
+    packet_periods_packet.packet_periods[1].packet_id = packet_id_quaternion_orientation;
     packet_periods_packet.packet_periods[1].period = 20; // 50 Hz
-    packet_periods_packet.packet_periods[2].packet_id = packet_id_quaternion_orientation;
+    packet_periods_packet.packet_periods[2].packet_id = packet_id_acceleration;
     packet_periods_packet.packet_periods[2].period = 20; // 50 Hz
-    packet_periods_packet.packet_periods[3].packet_id = packet_id_acceleration;
+    packet_periods_packet.packet_periods[3].packet_id = packet_id_angular_velocity;
     packet_periods_packet.packet_periods[3].period = 20; // 50 Hz
-    packet_periods_packet.packet_periods[4].packet_id = packet_id_angular_velocity;
-    packet_periods_packet.packet_periods[4].period = 20; // 50 Hz
+
+    packet_periods_packet.packet_periods[4].packet_id = packet_id_status;
+    packet_periods_packet.packet_periods[4].period = 200; // 5 Hz
+
     packet_periods_packet.packet_periods[5].packet_id = 0;
     an_packet = encode_packet_periods_packet(&packet_periods_packet);
     an_packet_encode_and_send(an_packet);
     an_packet_free(&an_packet);
 
-    imu_pub_ = nh.advertise<sensor_msgs::Imu>("imu/data", 1000);
+    imu_pub_ = nh.advertise<sensor_msgs::Imu>("imu/data", 10);
+
+    diagnostic_.setHardwareID("orientus");
+    diagnostic_.add("IMU Status", this, &OrientusNode::deviceStatus);
+    diagnostic_.add("Accelerometer Status", this, &OrientusNode::accelerometerStatus);
+    diagnostic_.add("Gyroscope Status", this, &OrientusNode::gyroscopeStatus);
+    diagnostic_.add("Magnetometer Status", this, &OrientusNode::magnetometerStatus);
+    diagnostic_.add("Temperature Status", this, &OrientusNode::temperatureStatus);
+    diagnostic_.add("Voltage Status", this, &OrientusNode::voltageStatus);
+    diagnostic_.add("Filter Status", this, &OrientusNode::filterStatus);
 
     an_decoder_initialise(&an_decoder_);
 
@@ -96,6 +110,7 @@ public:
 	}
 
 	if(status_received_){
+	  diagnostic_.update();
 	  status_received_ = false;
 	}
       }
@@ -177,6 +192,71 @@ private:
 	an_packet_free(&an_packet);
       }
     }
+  }
+
+  void deviceStatus(diagnostic_updater::DiagnosticStatusWrapper &status) {
+    if(status_packet_.system_status.b.system_failure)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::ERROR, "System Failure");
+    else if(status_packet_.system_status.b.serial_port_overflow_alarm)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::WARN, "Serial Port Overflow");
+    else
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::OK, "IMU is OK");
+
+    status.add("Device", port_name_);
+    status.add("TF frame", frame_id_);
+  }
+  void accelerometerStatus(diagnostic_updater::DiagnosticStatusWrapper &status) {
+    if(status_packet_.system_status.b.accelerometer_sensor_failure)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::ERROR, "Accelerometer Sensor Failure");
+    else if(status_packet_.system_status.b.accelerometer_over_range)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::WARN, "Accelerometer Sensor Over Range");
+    else
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::OK, "Accelerometer OK");
+  }
+  void gyroscopeStatus(diagnostic_updater::DiagnosticStatusWrapper &status) {
+    if(status_packet_.system_status.b.gyroscope_sensor_failure)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::ERROR, "Gyroscope Sensor Failure");
+    else if(status_packet_.system_status.b.gyroscope_over_range)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::WARN, "Gyroscope Sensor Over Range");
+    else
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::OK, "Gyroscope OK");
+  }
+  void magnetometerStatus(diagnostic_updater::DiagnosticStatusWrapper &status) {
+    if(status_packet_.system_status.b.magnetometer_sensor_failure)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::ERROR, "Magnetometer Sensor Failure");
+    else if(status_packet_.system_status.b.magnetometer_over_range)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::WARN, "Magnetometer Sensor Over Range");
+    else
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::OK, "Magnetometer OK");
+  }
+  void temperatureStatus(diagnostic_updater::DiagnosticStatusWrapper &status) {
+    if(status_packet_.system_status.b.minimum_temperature_alarm)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::WARN, "Minimum Temperature Alarm");
+    else if(status_packet_.system_status.b.maximum_temperature_alarm)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::WARN, "Maximum Temperature Alarm");
+    else
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::OK, "Temperature OK");
+  }
+  void voltageStatus(diagnostic_updater::DiagnosticStatusWrapper &status) {
+    if(status_packet_.system_status.b.low_voltage_alarm)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::WARN, "Low Voltage");
+    else if(status_packet_.system_status.b.high_voltage_alarm)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::WARN, "High Voltage");
+    else
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::OK, "Voltage OK");
+  }
+  void filterStatus(diagnostic_updater::DiagnosticStatusWrapper &status) {
+    if(status_packet_.filter_status.b.orientation_filter_initialised)
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::OK, "Filter OK");
+    else
+      status.summary(diagnostic_updater::DiagnosticStatusWrapper::ERROR, "Filter Not Initialised");
+
+    status.add("Heading Initialised", (bool)status_packet_.filter_status.b.heading_initialised);
+    status.add("Magnetometers Enabled", (bool)status_packet_.filter_status.b.magnetometers_enabled);
+    status.add("Velocity Heading Enabled", (bool)status_packet_.filter_status.b.velocity_heading_enabled);
+    status.add("External Position Active", (bool)status_packet_.filter_status.b.external_position_active);
+    status.add("External Velocity Active", (bool)status_packet_.filter_status.b.external_velocity_active);
+    status.add("External Heading Active", (bool)status_packet_.filter_status.b.external_heading_active);
   }
 
   void publish_imu_msg() {
